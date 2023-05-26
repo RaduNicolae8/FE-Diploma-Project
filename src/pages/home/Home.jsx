cc9393d2fabc78ef6c7dd23a6965aaab3010e93c
Radu Petrachioiu <radunicolae14@yahoo.com>
2023-05-27 02:56:13 +0300

    home page updates

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,8 +14,46 @@ const Home = () => {
           <CatCard key={card.id} item={card}/>
         ))}
       </Slide>
-      <br />
-        <br /><br /><br /><br /><br /><br />
+
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of co-production at your fingertips</h1>
+            <div className="title">
+              <img src="src\images\check2.png" alt="" />
+              Intuitive design and easeness of use
+              
+            </div>
+            <p>
+              Find high-quality services at every price point in a matter of clicks.
+            </p>
+            <div className="title">
+              <img src="src\images\check2.png" alt="" />
+              Reach your potential
+            </div>
+            <p>
+              Offer other users part of your expertise and start earning money today.
+            </p>
+            <div className="title">
+              <img src="src\images\check2.png" alt="" />
+              Adaptable to your needs
+            </div>
+            <p>
+              Did not find the service you were looking for? Go ahead and create a service request for it.
+            </p>
+            <div className="title">
+              <img src="src\images\check2.png" alt="" />
+              Centralized experience
+            </div>
+            <p>
+              Eliminate the need for hours of research and browsing through multiple websites.
+            </p>
+          </div>
+          <div className="item">
+            <img src="src\images\happy.jpg" alt="happy :)" className='happy'/>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
