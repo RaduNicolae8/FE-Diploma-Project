479e46bd69d94e018ad64c04504cb2dc5514e65a
Radu Petrachioiu <radunicolae14@yahoo.com>
2023-06-01 22:42:49 +0300

    services feature

---
+++ src/components/serviceCard/ServiceCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './ServiceCard.scss'
+import { Link } from 'react-router-dom'
+
+function ServiceCard({item}) {
+  return (
+    <Link to = "/service/123">
+    <div className='serviceCard'>
+        <img src={item.img} alt="" />
+        <div className="info">
+            <div className="user">
+                <span>{item.username}</span>
+            </div>
+            <p>{item.desc}</p>
+            <div className="star">
+                <img src="src\images\star.png" alt="" />
+                <span>{item.star}</span>
+            </div>
+        </div>
+        <hr />
+        <div className="details">
+            <img src="src\images\heart.png" alt="" />
+            <div className="price">
+
+            <span>Starting at</span>
+            <h2>{item.price} RON</h2>
+            </div>
+        </div>
+    </div>
+    </Link>
+  )
+}
+
+export default ServiceCard
--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import './Services.scss'
-
+import ServiceCard from '../../components/serviceCard/ServiceCard';
+import { services } from '../../data';
 
 function Services() {
+
+  const [open, setOpen] = React.useState(false);
+  const [sort, setSort] = React.useState("sales");
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  }
+
+
   return (
-    <div className='services'>Services</div>
+    <div className='services'>
+      <div className="container">
+        {/*span?  */}
+        <h1>Category Name</h1>
+        <p>
+          Category description Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder='min'/>
+            <input type="text" placeholder='max'/>
+            <button>Apply</button>
+
+          </div>
+          <div className="right">
+            <span className='sortBy'>Sort by</span>
+            <span className="sortType">{sort === "sales" ? "Best Selling" : "Newest"}</span>
+            <img src="src\images\down.png" alt="" onClick={()=>setOpen(!open)}/>
+            {open && <div className="rightMenu">
+              {sort === "sales" ? ( <span onClick={()=>reSort("createdAt")}>Newest</span> ) : 
+              ( <span onClick={()=>reSort("sales")}>Best Selling</span> )}
+            </div>}
+          </div>
+        </div>
+        <div className="cards">
+          {services.map(service => (
+            <ServiceCard key={service.id} item={service}/>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
