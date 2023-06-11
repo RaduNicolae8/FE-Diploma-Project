8d2cebbab90c35bcd6531d9027e7e9a92008f206
Radu <radunicolae14@yahoo.com>
2023-06-11 18:51:39 +0300

    service page updates

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -1,9 +1,115 @@
 import React from 'react'
 import './Service.scss'
+import Slide from '../../components/slide/Slide'
 
 function Service() {
   return (
-    <div className='service'>Service</div>
+    <div className='service'>
+      <div className="container">
+        <div className="left">
+          <h1>Service title </h1>
+          <div className="user">
+            <span>John Doe</span>
+            <div className="stars">
+              <img src="src\images\star.png" alt="star" />
+              <img src="src\images\star.png" alt="star" />
+              <img src="src\images\star.png" alt="star" />
+              <img src="src\images\star.png" alt="star" />
+              <img src="src\images\star.png" alt="heart" />
+              <span>5</span>
+            </div>
+          </div>
+          <Slide slidesToShow={1} arrowScroll={1}>
+            <img
+              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <img
+              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <img
+              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+          </Slide>
+          <h2>Service Description</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate saepe deserunt ea ipsam dolorum, eius fugiat doloremque enim beatae aliquid modi, obcaecati impedit voluptas aperiam ipsum dignissimos. Similique, eligendi voluptate.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto quasi omnis numquam velit? Sed placeat officiis delectus quisquam tempore, est doloribus id officia, autem asperiores, voluptate molestiae nostrum repellendus obcaecati?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magnam ab adipisci dolore maiores, enim dolor, animi veritatis quis, quos ratione dolorum et. Culpa consequatur, exercitationem sit eligendi dicta cupiditate.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem cumque magnam assumenda nesciunt earum voluptates, obcaecati facilis, deleniti delectus voluptatem dolore sapiente nobis. Quisquam tempora fugiat quo sunt iure porro.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi architecto assumenda neque explicabo provident temporibus, deserunt aut blanditiis autem cumque rerum, accusantium maiores dolorum iste cum dolores dolore recusandae laborum?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, minima eos nulla harum iure optio ratione, velit facere est voluptatibus, corrupti rem exercitationem beatae dicta omnis eius culpa impedit aspernatur.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur laborum quam dolore, aliquam accusamus at corrupti officia error aut? Officia officiis rem dolore nisi velit minima ad! Nobis, sunt aspernatur?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus voluptatibus, at recusandae, maiores eligendi consectetur tempore maxime eius saepe laborum ipsam aspernatur repellendus illo ipsa dolorem cum? Consequatur, velit dolorem.
+          </p>
+
+          <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="item">
+              <div className="user">
+                <span>John Doe</span>
+              </div>
+              <div className="stars">
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="heart" />
+                <span>5</span>
+              </div>
+              <p>
+                I really enjoyed this service... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eaque, porro eos at est ducimus accusamus ratione quis quam veniam. Aliquam voluptates laboriosam porro voluptas vel sapiente ab consequuntur sunt!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sed rem expedita quia eligendi. Modi consectetur quisquam repellat fugit possimus fuga, voluptatibus error hic, voluptatum debitis soluta aperiam deserunt dolorum.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi repellat, doloremque incidunt maiores voluptates error quam numquam eligendi distinctio velit fugiat, necessitatibus beatae optio ipsum! Ipsum omnis cupiditate asperiores!
+              </p>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <span>John Doe</span>
+              </div>
+              <div className="stars">
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="heart" />
+                <span>5</span>
+              </div>
+              <p>
+                I really enjoyed this service... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eaque, porro eos at est ducimus accusamus ratione quis quam veniam. Aliquam voluptates laboriosam porro voluptas vel sapiente ab consequuntur sunt!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sed rem expedita quia eligendi. Modi consectetur quisquam repellat fugit possimus fuga, voluptatibus error hic, voluptatum debitis soluta aperiam deserunt dolorum.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi repellat, doloremque incidunt maiores voluptates error quam numquam eligendi distinctio velit fugiat, necessitatibus beatae optio ipsum! Ipsum omnis cupiditate asperiores!
+              </p>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <span>John Doe</span>
+              </div>
+              <div className="stars">
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="star" />
+                <img src="src\images\star.png" alt="heart" />
+                <span>5</span>
+              </div>
+              <p>
+                I really enjoyed this service... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eaque, porro eos at est ducimus accusamus ratione quis quam veniam. Aliquam voluptates laboriosam porro voluptas vel sapiente ab consequuntur sunt!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sed rem expedita quia eligendi. Modi consectetur quisquam repellat fugit possimus fuga, voluptatibus error hic, voluptatum debitis soluta aperiam deserunt dolorum.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi repellat, doloremque incidunt maiores voluptates error quam numquam eligendi distinctio velit fugiat, necessitatibus beatae optio ipsum! Ipsum omnis cupiditate asperiores!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
