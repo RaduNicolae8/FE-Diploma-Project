b1f7152d5b13a83cf509aab0da86bf0ea377cd56
Radu <radunicolae14@yahoo.com>
2023-06-19 15:36:53 +0300

    added service page

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -6,45 +6,106 @@ function Service() {
   return (
     <div className='service'>
       <div className="container">
+        <div className="leftright">
+
         <div className="left">
-          <h1>Service title </h1>
+          <h1>I will generate images for your website </h1>
           <div className="user">
-            <span>John Doe</span>
+            <span>Anna Bell</span>
             <div className="stars">
               <img src="src\images\star.png" alt="star" />
               <img src="src\images\star.png" alt="star" />
               <img src="src\images\star.png" alt="star" />
               <img src="src\images\star.png" alt="star" />
-              <img src="src\images\star.png" alt="heart" />
+              <img src="src\images\star.png" alt="star" />
               <span>5</span>
             </div>
           </div>
+
           <Slide slidesToShow={1} arrowScroll={1}>
             <img
               src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
+              alt="error"
+              />
             <img
               src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
+              alt="error"
+              />
             <img
               src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
+              alt="error"
+              />
           </Slide>
           <h2>Service Description</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate saepe deserunt ea ipsam dolorum, eius fugiat doloremque enim beatae aliquid modi, obcaecati impedit voluptas aperiam ipsum dignissimos. Similique, eligendi voluptate.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto quasi omnis numquam velit? Sed placeat officiis delectus quisquam tempore, est doloribus id officia, autem asperiores, voluptate molestiae nostrum repellendus obcaecati?
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magnam ab adipisci dolore maiores, enim dolor, animi veritatis quis, quos ratione dolorum et. Culpa consequatur, exercitationem sit eligendi dicta cupiditate.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem cumque magnam assumenda nesciunt earum voluptates, obcaecati facilis, deleniti delectus voluptatem dolore sapiente nobis. Quisquam tempora fugiat quo sunt iure porro.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi architecto assumenda neque explicabo provident temporibus, deserunt aut blanditiis autem cumque rerum, accusantium maiores dolorum iste cum dolores dolore recusandae laborum?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, minima eos nulla harum iure optio ratione, velit facere est voluptatibus, corrupti rem exercitationem beatae dicta omnis eius culpa impedit aspernatur.
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur laborum quam dolore, aliquam accusamus at corrupti officia error aut? Officia officiis rem dolore nisi velit minima ad! Nobis, sunt aspernatur?
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus voluptatibus, at recusandae, maiores eligendi consectetur tempore maxime eius saepe laborum ipsam aspernatur repellendus illo ipsa dolorem cum? Consequatur, velit dolorem.
+          As a seasoned graphic designer with over 10 years of experience, I specialize in creating captivating, custom images that will elevate your website to new heights. With my service, you can stand out in today's crowded digital landscape and effectively communicate your brand's story to your audience.<br /><br />
+          Service Features:<br />
+          Custom Designed Images: Say goodbye to stock images that just don't capture the essence of your brand. With my service, you get 100% custom designed images that are specifically tailored to your brand's aesthetics, mission, and objectives. <br />
+          High-Quality Graphics: All images will be delivered in high-resolution, ready for web or print use. They will look stunning on all screen sizes, from mobile devices to desktop monitors.<br />
+          Quick Turnaround Time: In today's fast-paced world, speed matters. That's why I promise a quick turnaround time, without compromising on quality.<br />
+          Unlimited Revisions: I want you to be completely satisfied with the final product. Therefore, I offer unlimited revisions until you are 100% happy with your images.<br />
+          Friendly Communication: As a client, you are a part of the design process. I keep an open line of communication to understand your needs and feedback throughout the project.<br /><br />
+          I work with businesses of all sizes, from startups to established companies, across a range of industries. Whether you need images for your homepage, product pages, blog posts, or social media, I've got you covered.<br />
+          Let me help you create a visually compelling website that resonates with your audience and drives results. Place an order today, and let's bring your vision to life!<br />
+          Please don't hesitate to message me if you have any questions. I'm always happy to discuss your project's needs and offer a bespoke solution that fits your budget and timeline.<br />
+          Stand out from the crowd. Stand out with custom designed images. I look forward to working with you!
           </p>
 
+         </div>
+         <div className="right">
+
+         
+          <div className="seller">
+            {/* <h2>About The Seller</h2> */}
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="info">
+                <span>Anna Bell</span>
+                <div className="stars">
+                  <img src="src\images\star.png" alt="" />
+                  <img src="src\images\star.png" alt="" />
+                  <img src="src\images\star.png" alt="" />
+                  <img src="src\images\star.png" alt="" />
+                  <img src="src\images\star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <button>Contact Me</button>
+              </div>
+            </div>
+            <div className="box">
+              <div className="items">
+                <div className="item">
+                  <span className="title">From</span>
+                  <span className="desc">USA</span>
+                </div>
+                <div className="item">
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
+                </div>
+                <div className="item">
+                  <span className="title">Avg. response time</span>
+                  <span className="desc">4 hours</span>
+                </div>
+                <div className="item">
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
+                </div>
+                <div className="item">
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
+                </div>
+              </div>
+              <hr />
+              <p>
+              Hello there! I'm Anna, a passionate graphic designer with over 10 years of experience. I've helped businesses worldwide to captivate their audience with unique and high-quality images. Let me transform your website into a visually stunning platform that resonates with your brand's story!
+              </p>
+            </div>
+          </div>
+          </div>
+          </div>
           <div className="reviews">
             <h2>Reviews</h2>
             <div className="item">
@@ -60,15 +121,12 @@ function Service() {
                 <span>5</span>
               </div>
               <p>
-                I really enjoyed this service... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eaque, porro eos at est ducimus accusamus ratione quis quam veniam. Aliquam voluptates laboriosam porro voluptas vel sapiente ab consequuntur sunt!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sed rem expedita quia eligendi. Modi consectetur quisquam repellat fugit possimus fuga, voluptatibus error hic, voluptatum debitis soluta aperiam deserunt dolorum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi repellat, doloremque incidunt maiores voluptates error quam numquam eligendi distinctio velit fugiat, necessitatibus beatae optio ipsum! Ipsum omnis cupiditate asperiores!
-              </p>
+              "Anna is a top-notch designer! She delivered high-quality images for our website, truly capturing the essence of our brand. Fast turnaround and excellent communication throughout the project. Highly recommend!"              </p>
             </div>
             <hr />
             <div className="item">
               <div className="user">
-                <span>John Doe</span>
+                <span>Jane Doe</span>
               </div>
               <div className="stars">
                 <img src="src\images\star.png" alt="star" />
@@ -79,15 +137,13 @@ function Service() {
                 <span>5</span>
               </div>
               <p>
-                I really enjoyed this service... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eaque, porro eos at est ducimus accusamus ratione quis quam veniam. Aliquam voluptates laboriosam porro voluptas vel sapiente ab consequuntur sunt!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sed rem expedita quia eligendi. Modi consectetur quisquam repellat fugit possimus fuga, voluptatibus error hic, voluptatum debitis soluta aperiam deserunt dolorum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi repellat, doloremque incidunt maiores voluptates error quam numquam eligendi distinctio velit fugiat, necessitatibus beatae optio ipsum! Ipsum omnis cupiditate asperiores!
+              "Absolutely thrilled with the images Anna created for our website. Her creative flair and attention to detail are exceptional. She was always ready to make revisions until we were completely satisfied. Fantastic work!"
               </p>
             </div>
             <hr />
             <div className="item">
               <div className="user">
-                <span>John Doe</span>
+                <span>Adam Smith</span>
               </div>
               <div className="stars">
                 <img src="src\images\star.png" alt="star" />
@@ -98,17 +154,12 @@ function Service() {
                 <span>5</span>
               </div>
               <p>
-                I really enjoyed this service... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eaque, porro eos at est ducimus accusamus ratione quis quam veniam. Aliquam voluptates laboriosam porro voluptas vel sapiente ab consequuntur sunt!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sed rem expedita quia eligendi. Modi consectetur quisquam repellat fugit possimus fuga, voluptatibus error hic, voluptatum debitis soluta aperiam deserunt dolorum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis animi repellat, doloremque incidunt maiores voluptates error quam numquam eligendi distinctio velit fugiat, necessitatibus beatae optio ipsum! Ipsum omnis cupiditate asperiores!
+              "Working with Anna was a game-changer for our website's visuals. Her designs are not only beautiful but they also align perfectly with our branding. Her professionalism and dedication are second to none. Can't wait to start our next project with her!"
               </p>
             </div>
           </div>
         </div>
-        <div className="right">
-
-        </div>
-      </div>
+      
     </div>
   )
 }
