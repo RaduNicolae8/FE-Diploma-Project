aa378ccba2d371e1aef442b1dbc898eef07f0a43
Radu <radunicolae14@yahoo.com>
2023-07-04 15:43:09 +0300

    tweaks

--- src/pages/myServices/MyServices.jsx
+++ src/pages/myServices/MyServices.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 import './MyServices.scss'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../App'
+import newRequest from '../../utils/newRequest'
+import { useQuery } from 'react-query'
+import { useState } from 'react'
 
 function MyServices() {
+
+  const context = useContext(AuthContext);
+
+  const authUser = context.authUser;
+  const [services, setServices] = useState([{}]);
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["servicesbyid"],
+    queryFn: () =>{
+  
+      newRequest.get('/api/service/get/user',{params : {userId:authUser.id}}).then((res) => {
+          
+          setServices(res.data);
+          console.log(res);
+          //console.log(services);
+      
+      })
+  
+    },
+  });
+  //console.log(services);
+
+  const deleteService = async (id) => {
+    await newRequest.delete('/api/images/delete',{params:{id:id}})
+    await newRequest.delete('/api/service/'+id)
+    window.location.reload();
+  }
+
   return (
 <div className='myServices'>
 <div className="container">
@@ -17,75 +50,22 @@ function MyServices() {
      <th>Image</th> 
      <th>Title</th> 
      <th>Price</th> 
-     <th>Orders</th> 
      <th>Action</th> 
     </tr>
-    <tr>
-      <td>
-        <img className='img' src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-      </td>
-      <td>Service1</td>
-      <td>120</td>
-      <td>8</td>
-      <td>
-        <img className='delete' src="src\images\delete.png" alt="" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img className='img' src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-      </td>
-      <td>Service1</td>
-      <td>120</td>
-      <td>8</td>
-      <td>
-        <img className='delete' src="src\images\delete.png" alt="" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img className='img' src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-      </td>
-      <td>Service1</td>
-      <td>120</td>
-      <td>8</td>
-      <td>
-        <img className='delete' src="src\images\delete.png" alt="" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img className='img' src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-      </td>
-      <td>Service1</td>
-      <td>120</td>
-      <td>8</td>
-      <td>
-        <img className='delete' src="src\images\delete.png" alt="" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img className='img' src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-      </td>
-      <td>Service1</td>
-      <td>120</td>
-      <td>8</td>
-      <td>
-        <img className='delete' src="src\images\delete.png" alt="" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <img className='img' src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-      </td>
-      <td>Service1</td>
-      <td>120</td>
-      <td>8</td>
-      <td>
-        <img className='delete' src="src\images\delete.png" alt="" />
-      </td>
-    </tr>
+    {
+      services.map((service) => (
+        <tr>
+        <td>
+          <img className='img' src={service.coverImage} alt="" />
+        </td>
+        <td>{service.title}</td>
+        <td>{service.price}</td>
+        <td>
+          <img className='delete' src="src\images\delete.png" alt="" onClick={()=>deleteService(service.id)}/>
+        </td>
+      </tr>
+      ))
+    }
   </table>
 </div>
 </div>
